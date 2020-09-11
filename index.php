b99b2af1814e46f0aeae7862c9b8aca00a4c1875
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-11 18:15:28 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -29,9 +29,7 @@ EOF;
         }
         while($row = pg_fetch_row($ret)) {
           echo("<br>");
-          echo("http://api.hgbrasil.com/finance/stock_price?key=4750432b&symbol="+$row[0]);
-          echo("<br>");
-          $url  = "http://api.hgbrasil.com/finance/stock_price?key=4750432b&symbol="+$row[0];
+          $url  = "http://api.hgbrasil.com/finance/stock_price?key=4750432b&symbol=".$row[0];
           $ch = curl_init();
           curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
