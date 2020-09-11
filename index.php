ab48563396dc8c6d4b47dd74378a9587fd62372b
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-11 20:21:50 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ EOF;
           $comprar = array();
           $estavel = array();
           $acao =  array($dados['results'][$row[0]]['symbol'], $dados['results'][$row[0]]['price'], $dados['results'][$row[0]]['change_percent']);
-          echo($dados['results'][$row[0]]['symbol']);
+          echo "Ação: ".$dados['results'][$row[0]]['symbol'];
           if($dados['results'][$row[0]]['price']>1){
             array_push($vender, $acao);
           }
