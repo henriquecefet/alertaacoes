8f995852525466ad969e6090e10a07d5cf1c23a5
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-11 17:57:26 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 $host        = "host = ec2-54-166-107-5.compute-1.amazonaws.com";
 $port        = "port = 5432";
 $dbname      = "dbname = db5pscbsc5u5km";
-$credentials = "user = hmqbzhhxovmzw password=ffa8e598fe771f7b2350a22358d122abf6f87763f7ddbde9dc2d35e523b97802";
+$credentials = "user = hmqbzhhxovmzwl password=ffa8e598fe771f7b2350a22358d122abf6f87763f7ddbde9dc2d35e523b97802";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
