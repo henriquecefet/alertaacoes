128fd239abdfeadc8657d0112a43b2506580058e
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-11 15:01:36 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-$response = file_get_contents('https://api.hgbrasil.com/finance/stock_price?key=4750432b&symbol=b3sa3')
-$response = json_decode($response);
-echo($response)
-
-;
+//$response = file_get_contents('https://api.hgbrasil.com/finance/stock_price?key=4750432b&symbol=b3sa3')
+//$response = json_decode($response);
+//echo($response);
+echo("ola");
 ?>
