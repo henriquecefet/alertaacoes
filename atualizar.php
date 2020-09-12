6c980f60f79144306afcfed9bc01a7467c70d2af
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-12 11:31:35 -0300

    Add files via upload

---
+++ atualizar.php
@@ -0,0 +1,46 @@
+<?php
+$sql =<<<EOF
+   SELECT * from acoes.acoes;
+EOF;
+$ret = pg_query($db, $sql);
+if(!$ret) {
+     echo pg_last_error($db);
+ exit;
+}
+while($row = pg_fetch_row($ret)) {
+  $url  = "http://api.hgbrasil.com/finance/stock_price?key=8a9cc248&symbol=".$row[0];
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_URL,$url);
+  $result=curl_exec($ch);
+  curl_close($ch);
+  $dados = json_decode($result, true);
+  $nome = $dados['results'][$row[0]]['symbol'];
+  $preco = $dados['results'][$row[0]]['price'];
+  $variacao = $dados['results'][$row[0]]['change_percent'];
+  if($variacao>1){
+    $recomendacao = 1; // vender
+  }
+  elseif($variacao<-1){
+    $recomendacao = 2; // comprar
+  }
+  else{
+      $recomendacao = 3; // estável
+  }
+  $sql =<<<EOF
+     UPDATE acoes.acoes SET nome = $nome, preco = $preco, variacao = $variacao, recomendacao = $recomendacao;
+EOF;
+  $ret = pg_query($db, $sql);
+  if(!$ret) {
+       echo pg_last_error($db);
+       exit;
+  }
+  else{
+    echo "Banco atualizado";
+  }
+
+}
+//var_dump($comprar);
+pg_close($db);
+?>
