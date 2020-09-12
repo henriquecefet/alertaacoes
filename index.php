d6b581b82f8a15f29b6d586b9be5fddabfb34a34
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-12 12:23:52 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $estavel = array();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     </head>
-    <body>
+    <body style="background-color:#f8f9fa">
     <div class="jumbotron text-center">
     <h1>Alerta de Ações</h1>
        <p>Lista de Ações da B3</p>
