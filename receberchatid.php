22902c062dbec1f8f3a33c357d0716f1a81744a8
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 19:58:16 -0300

    Add files via upload

--- receberchatid.php
+++ receberchatid.php
@@ -1,12 +1,13 @@
 <?php
-include("conexao.php");
-include("funcaotelegram.php");
+//include("conexao.php");
+//include("funcaotelegram.php");
 
-$update = json_decode(file_get_contents("php://input"), TRUE);
+//$update = json_decode(file_get_contents("php://input"), TRUE);
 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
-telegram("Olá, tudo bem?",$chatId);
+//$chatId = $update["message"]["chat"]["id"];
+//$message = $update["message"]["text"];
+http_response_code(200);
+//telegram("Olá, tudo bem?",$chatId);
 /*
 if($message == "ações"){
   mandarAcoesParaUmaPessoa($chatId);
