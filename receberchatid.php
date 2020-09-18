5606f1401c1701141512241fa1ab396a163413eb
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 19:41:48 -0300

    Add files via upload

--- receberchatid.php
+++ receberchatid.php
@@ -1,22 +1,23 @@
 <?php
 include("conexao.php");
 include("funcaotelegram.php");
-$path = "https://api.telegram.org/bot1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o";
 
 $update = json_decode(file_get_contents("php://input"), TRUE);
 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
+telegram("Olá, tudo bem?",$chatId);
+/*
 if($message == "ações"){
-mandarAcoesParaUmaPessoa($chatId);
+  mandarAcoesParaUmaPessoa($chatId);
 }
 elseif($message == "salvar"){
-adicionarChat($chatId);
+  adicionarChat($chatId);
 }else{
-telegram("Olá, tudo bem?
-Digite 'ações' para receber recomendações de ações agora.
- Digite 'salvar' para reber recomendações de ações ao longo do dia",$chatId);
-}
-
+  telegram("Olá, tudo bem?
+  Digite 'ações' para receber recomendações de ações agora.
+  Digite 'salvar' para reber recomendações de ações ao longo do dia",$chatId);
+  }
+*/
 
 ?>
