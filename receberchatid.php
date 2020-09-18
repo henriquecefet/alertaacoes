f90ba623571631827007cce6f7d748f748b8aeac
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 19:21:02 -0300

    Add files via upload

--- receberchatid.php
+++ receberchatid.php
@@ -1,7 +1,6 @@
 <?php
 include("conexao.php");
 include("funcaotelegram.php");
-$path = "https://api.telegram.org/bot1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o";
 
 $update = json_decode(file_get_contents("php://input"), TRUE);
 
