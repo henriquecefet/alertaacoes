6679bd80dbd113f0fda2c5f63a2663b1e76a961b
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 17:53:57 -0300

    Add files via upload

--- receberchatid.php
+++ receberchatid.php
@@ -1,24 +1,9 @@
 <?php
-include("conexao.php");
-include("funcaotelegram.php");
-$url  = "https://api.telegram.org/bot1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o/getUpdates";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url);
-$result=curl_exec($ch);
-curl_close($ch);
-$dados = json_decode($result, true);
-$chatid = $dados['result'][0]['message']['chat']['id'];
-$sql =<<<EOF
-   INSERT INTO acoes.usuarios (chatid) VALUES ('$chatid');
-EOF;
-$ret = pg_query($db, $sql);
-if(!$ret) {
-     echo pg_last_error($db);
-}
-else{
-  echo "Banco atualizado";
-  telegram("Seu numero foi registrado. Você receberá recomendações de ações.");
-}
+$path = "https://api.telegram.org/bot1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o";
+
+$update = json_decode(file_get_contents("php://input"), TRUE);
+
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+telegram("Olá, tudo bem?");
  ?>
