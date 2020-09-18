2d188356c0e783260dad277bc6cee85b16b69cc8
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 18:21:48 -0300

    Add files via upload

--- funcaotelegram.php
+++ funcaotelegram.php
@@ -1,4 +1,5 @@
 <?php
+include("conexao.php");
 function telegram($msg, $telegramchatid) {
         $telegrambot='1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o';
         $url='https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data=array('chat_id'=>$telegramchatid,'text'=>$msg);
@@ -6,6 +7,38 @@ function telegram($msg, $telegramchatid) {
         $context=stream_context_create($options);
         $result=file_get_contents($url,false,$context);
         return $result;
+}
+function mandarAcoesParaUmaPessoa($chatid){
+  $sql1 =<<<EOF
+     SELECT * from acoes.acoes;
+  EOF;
+  $ret1 = pg_query($db, $sql1);
+  if(!$ret1) {
+       echo pg_last_error($db);
+   exit;
+  }
+  while($row = pg_fetch_row($ret1)) {
+      if($row[2]>3){
+          telegram("Recomendação de venda: ".$row[0].", com preço de R$".$row[1]." e variação de ".$row[2]."%",$chatid);
+        }
+
+
+      elseif($row[2]<-3){
+        telegram("Recomendação de compra: ".$row[0].", com preço de R$".$row[1]." e variação de ".$row[2]."%",$chatid);
+      }
+  }
+}
+function adicionarChat($chatid){
+  $sql =<<<EOF
+     INSERT INTO acoes.usuarios (chatid) VALUES ('$chatid');
+  EOF;
+  $ret = pg_query($db, $sql);
+  if(!$ret) {
+       echo pg_last_error($db);
+  }
+  else{
+    telegram("Seu numero foi registrado. Você receberá recomendações de ações.",$chatid);
+  }
 }
 
  ?>
--- receberchatid.php
+++ receberchatid.php
@@ -6,5 +6,16 @@ $update = json_decode(file_get_contents("php://input"), TRUE);
 
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-telegram("Olá, tudo bem?",$chatId);
+if($message == "ações"){
+  mandarAcoesParaUmaPessoa($chatId);
+}
+elseif($message == "salvar"){
+  adicionarChat($chatId);
+}else{
+  telegram("Olá, tudo bem?
+  Digite 'ações' para receber recomendações de ações agora.
+   Digite 'salvar' para reber recomendações de ações ao longo do dia",$chatId);
+}
+
+
  ?>
