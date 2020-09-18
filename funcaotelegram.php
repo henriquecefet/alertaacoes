1f19156e771c9c80680a24b8eb35f2b7fa45be91
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 15:36:57 -0300

    Add files via upload

---
+++ funcaotelegram.php
@@ -0,0 +1,11 @@
+<?php
+function telegram($msg, $telegramchatid) {
+        $telegrambot='1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o';
+        $url='https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data=array('chat_id'=>$telegramchatid,'text'=>$msg);
+        $options=array('http'=>array('method'=>'POST','header'=>"Content-Type:application/x-www-form-urlencoded\r\n",'content'=>http_build_query($data),),);
+        $context=stream_context_create($options);
+        $result=file_get_contents($url,false,$context);
+        return $result;
+}
+
+ ?>
--- mandartelegram.php
+++ mandartelegram.php
@@ -1,29 +1,35 @@
 <?php
 include("conexao.php");
-$telegrambot='1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o';
-$telegramchatid=1037497721;
-function telegram($msg) {
-        global $telegrambot,$telegramchatid;
-        $url='https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data=array('chat_id'=>$telegramchatid,'text'=>$msg);
-        $options=array('http'=>array('method'=>'POST','header'=>"Content-Type:application/x-www-form-urlencoded\r\n",'content'=>http_build_query($data),),);
-        $context=stream_context_create($options);
-        $result=file_get_contents($url,false,$context);
-        return $result;
+include("funcaotelegram.php");
+$chatids = array();
+$sql2 =<<<EOF
+   SELECT * from acoes.usuarios;
+EOF;
+$ret2 = pg_query($db, $sql2);
+if(!$ret) {
+     echo pg_last_error($db);
 }
-$sql =<<<EOF
+while($row2 = pg_fetch_row($ret2)) {
+    array_push($chatids, $row2[0]);
+}
+$sql1 =<<<EOF
    SELECT * from acoes.acoes;
 EOF;
-$ret = pg_query($db, $sql);
-if(!$ret) {
+$ret1 = pg_query($db, $sql1);
+if(!$ret1) {
      echo pg_last_error($db);
  exit;
 }
-while($row = pg_fetch_row($ret)) {
-  if($row[2]>3){
-    telegram("Recomendação de venda: ".$row[0].", com preço de R$".$row[1]." e variação de ".$row[2]."%");
-  }
-  elseif($row[2]<-3){
-    telegram("Recomendação de compra: ".$row[0].", com preço de R$".$row[1]." e variação de ".$row[2]."%");
+while($row = pg_fetch_row($ret1)) {
+  for($i = 0; $i < sizeof($chatids); $i = $i + 1){
+    if($row[2]>3){
+        telegram("Recomendação de venda: ".$row[0].", com preço de R$".$row[1]." e variação de ".$row[2]."%",$chatids[$i]);
+      }
+
+
+    elseif($row[2]<-3){
+      telegram("Recomendação de compra: ".$row[0].", com preço de R$".$row[1]." e variação de ".$row[2]."%",$chatids[$i]);
+    }
   }
 
 }
