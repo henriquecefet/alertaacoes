d8d1a4c08a38938ce84377c20962bb91ccf6f912
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 19:31:05 -0300

    Add files via upload

--- receberchatid.php
+++ receberchatid.php
@@ -1,29 +1,22 @@
 <?php
 include("conexao.php");
 include("funcaotelegram.php");
+$path = "https://api.telegram.org/bot1119671047:AAFTHvMowYex6dRol8_-d1eHy3kbe3Siv_o";
 
 $update = json_decode(file_get_contents("php://input"), TRUE);
 
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+if($message == "ações"){
+mandarAcoesParaUmaPessoa($chatId);
+}
+elseif($message == "salvar"){
+adicionarChat($chatId);
+}else{
+telegram("Olá, tudo bem?
+Digite 'ações' para receber recomendações de ações agora.
+ Digite 'salvar' para reber recomendações de ações ao longo do dia",$chatId);
+}
 
-if( !$update ){
-    telegram("Some Error output (request is not valid JSON)",1037497721);
-}
-elseif( !isset($update['update_id']) || !isset($update['message']) ){
-    telegram("Some Error output (request has not message)",1037497721);
 
-}
-else{
-  $chatId = $update["message"]["chat"]["id"];
-  $message = $update["message"]["text"];
-  if($message == "ações"){
-    mandarAcoesParaUmaPessoa($chatId);
-  }
-  elseif($message == "salvar"){
-    adicionarChat($chatId);
-  }else{
-    telegram("Olá, tudo bem?
-    Digite 'ações' para receber recomendações de ações agora.
-     Digite 'salvar' para reber recomendações de ações ao longo do dia",$chatId);
-  }
-}
- ?>
+?>
