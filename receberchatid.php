3c3a190163adf50986d9d58cd6cbb7642ed43049
henriquecefet <rodriguesoareshenrique@gmail.com>
2020-09-18 19:26:09 -0300

    Add files via upload

--- receberchatid.php
+++ receberchatid.php
@@ -4,19 +4,26 @@ include("funcaotelegram.php");
 
 $update = json_decode(file_get_contents("php://input"), TRUE);
 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
-telegram("Olá",$chatId);
-if($message == "ações"){
-  mandarAcoesParaUmaPessoa($chatId);
-}
-elseif($message == "salvar"){
-  adicionarChat($chatId);
-}else{
-  telegram("Olá, tudo bem?
-  Digite 'ações' para receber recomendações de ações agora.
-   Digite 'salvar' para reber recomendações de ações ao longo do dia",$chatId);
-}
 
+if( !$update ){
+    telegram("Some Error output (request is not valid JSON)",1037497721);
+}
+elseif( !isset($update['update_id']) || !isset($update['message']) ){
+    telegram("Some Error output (request has not message)",1037497721);
 
+}
+else{
+  $chatId = $update["message"]["chat"]["id"];
+  $message = $update["message"]["text"];
+  if($message == "ações"){
+    mandarAcoesParaUmaPessoa($chatId);
+  }
+  elseif($message == "salvar"){
+    adicionarChat($chatId);
+  }else{
+    telegram("Olá, tudo bem?
+    Digite 'ações' para receber recomendações de ações agora.
+     Digite 'salvar' para reber recomendações de ações ao longo do dia",$chatId);
+  }
+}
  ?>
